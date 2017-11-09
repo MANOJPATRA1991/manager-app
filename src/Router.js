566672cdd06d2cab395a672d5dd87f15a2024d32
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-09 13:36:58 +0530

    Test routing functionality

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Stack, Scene, Router } from 'react-native-router-flux';
 import LoginFrom from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
     return (
         <Router>
-            <Stack key="root">
-                <Scene key="login" component={LoginFrom} title="Please Login" />    
-            </Stack>
+            <Scene key="root" hideNavBar>
+                <Scene key="auth">
+                    <Scene key="login" component={LoginFrom} title="Please Login" />    
+                </Scene>
+                <Scene key="main">
+                    <Scene key="employeeList" component={EmployeeList} title="Employees" />
+                </Scene>
+            </Scene>
         </Router>
     )
 };
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import { 
     EMAIL_CHANGED, 
     PASSWORD_CHANGED,
@@ -23,6 +25,7 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
+        // dispatches action with type LOGIN_USER to all reducers
         dispatch({ type: LOGIN_USER });
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => {
@@ -47,6 +50,8 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     });
+
+    Actions.main();
 };
 
 const loginUserFail = (dispatch) => {
