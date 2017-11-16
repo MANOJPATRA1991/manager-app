d117efef804759e6fc88497dcc8bae4188e5feca
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-16 12:22:48 +0530

    Add docs

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -9,6 +9,11 @@ import {
     LOGIN_USER
 } from './types';
 
+/**
+ * Returns action to update state on email change
+ * @param {*} text Email input
+ * @returns Action
+ */
 export const emailChanged = (text) => {
     return {
         type: EMAIL_CHANGED,
@@ -16,6 +21,11 @@ export const emailChanged = (text) => {
     };
 };
 
+/**
+ * Returns action to update state on password change
+ * @param {*} text Password input
+ * @returns Action
+ */
 export const passwordChanged = (text) => {
     return {
         type: PASSWORD_CHANGED,
@@ -23,28 +33,40 @@ export const passwordChanged = (text) => {
     };
 };
 
+/**
+ * Logs in user to firebase
+ * @param {*} param0 An object with two keys: email and password
+ */
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
-        // dispatches action with type LOGIN_USER to all reducers
+        // Dispatches action with type LOGIN_USER to all reducers
         dispatch({ type: LOGIN_USER });
+        // Sign in user
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => {
+            // Dispatch action with user payload on login success
             loginUserSuccess(dispatch, user);   
         })
         .catch((error) => {
-            console.log(error);
-
+            // Create new user if user doesn't exist in database
             firebase.auth().createUserWithEmailAndPassword(email, password)
             .then(user => {
+                // Dispatch action with user payload on login success
                 loginUserSuccess(dispatch, user);
             })
             .catch(() => {
+                // Dispatch action stating login failure
                 loginUserFail(dispatch);
             });
         });
     };
 };
 
+/**
+ * Dispatch new Action with user data on login successs
+ * @param {*} dispatch  
+ * @param {*} user 
+ */
 const loginUserSuccess = (dispatch, user) => {
     dispatch({
         type: LOGIN_USER_SUCCESS,
@@ -54,6 +76,11 @@ const loginUserSuccess = (dispatch, user) => {
     Actions.main();
 };
 
+/**
+ * Dispatch new Action stating login failure
+ * @param {*} dispatch  
+ * @param {*} user 
+ */
 const loginUserFail = (dispatch) => {
     dispatch({ type: LOGIN_USER_FAIL });
 }
